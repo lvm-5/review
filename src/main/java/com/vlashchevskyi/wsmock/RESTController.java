073432d9b04e11cf82016d0873a9d20a05d5f3b0
lvm-5 <v.lashchevsky@gmail.com>
2017-02-26 19:43:56 -0500

    added post method handler

--- src/main/java/com/vlashchevskyi/wsmock/RESTController.java
+++ src/main/java/com/vlashchevskyi/wsmock/RESTController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by lvm on 2/21/17.
@@ -15,6 +16,11 @@ public class RESTController {
 
     @RequestMapping(method=GET)
     public String doGETMessage() throws InterruptedException{
+        return response;
+    }
+
+    @RequestMapping(method=POST)
+    public String doPOSTMessage() throws InterruptedException{
         return response;
     }
 }
